39e123bab04554a7be221160e4c5270542422182
Charlie BOYER <charlie.boyer2021@campus-eni.fr>
2021-06-04 16:23:28 +0200

    first commit

---
+++ src/main/java/com/example/potager/PotagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.potager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotagerApplication.class, args);
+	}
+
+}
