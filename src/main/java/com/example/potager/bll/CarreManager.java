459271a8cc601edbd4bab4dce148168ffc8c839d
CassandraVn <cassandra.vaslon2021@campus-eni.fr>
2021-06-07 10:32:17 +0200

    Debut carreManager

--- src/main/java/com/example/potager/bll/CarreManager.java
+++ src/main/java/com/example/potager/bll/CarreManager.java
@@ -1,5 +1,18 @@
 package com.example.potager.bll;
 
+import com.example.potager.bo.Carre;
+import com.example.potager.bo.Plante;
+
 public interface CarreManager {
 
+	public void addCarre(Carre carre);
+	
+	public void removeCarre(Carre carre);
+	
+	public void removeCarreById(Integer id);
+	
+	public void updateCarre(Carre carre);
+	
+	public Plante getNomPlante();
+	
 }
