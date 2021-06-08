6b848b4ff52477762c143a53c88fba0c1478ea00
Jeje-eni <82143171+Jeje-eni@users.noreply.github.com>
2021-06-08 18:26:33 +0200

    WS Carre by id

--- src/main/java/com/example/potager/ws/CarreController.java
+++ src/main/java/com/example/potager/ws/CarreController.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.potager.bll.CarreManager;
 import com.example.potager.bo.Carre;
+
 
 @RestController
 public class CarreController {
@@ -17,5 +18,11 @@ public class CarreController {
 	
 	@GetMapping("/api/getCarre")
 	public List<Carre> getCarre(){
-		return carreManager.getAllCarre();	}
+		return carreManager.getAllCarre();	
+		}
+	
+	@GetMapping("/WS/getCarre/{id}")
+	public Carre one(@PathVariable Integer id) {
+		return carreManager.getById(id);
+	}
 }
