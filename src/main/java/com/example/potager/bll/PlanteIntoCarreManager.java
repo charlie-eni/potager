15013daf58be85c11ad061f470e6d960dafe745e
CassandraVn <cassandra.vaslon2021@campus-eni.fr>
2021-06-08 10:01:06 +0200

    PlanteIntoCarre manager et impl
    pour le delete

---
+++ src/main/java/com/example/potager/bll/PlanteIntoCarreManager.java
@@ -0,0 +1,11 @@
+package com.example.potager.bll;
+
+import com.example.potager.bo.PlanteIntoCarre;
+
+public interface PlanteIntoCarreManager {
+
+	public void deletePlanIntoCarre(PlanteIntoCarre plan);
+	
+	public void deletePlanIntoCarreById(Integer id);
+	
+}
